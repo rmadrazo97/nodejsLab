6c3654a8bb5348fb833c4754a7598a5b8ba3a495
rmadrazo97 <jmadrazo7@gmail.com>
2018-09-13 09:14:01 -0600

    finalizado

--- readAsyncCb.js
+++ readAsyncCb.js
@@ -3,26 +3,21 @@ var fs = require('fs')
 function callback  (err, data) {
     if (err) return console.error(err);
     var textPlain = data.toString()
-    var j = 0;
-    for (var i = 0; i <= textPlain.length; i++){
-        
-        //console.log(textPlain[i]);
-        if (textPlain[i] == '\n' ){
-            //console.log("LINEBREAK")
-            fs.appendFile('./fileTest', '\n Linea:'+j-- , function (err) {
-                if (err) throw err;
-              });
-            j = 0;
-        }
-        j++;
-    }
-    //console.log(textPlain)    
+    
+    var arreglo = textPlain.split('\n');
+
+    arreglo.forEach(elem => {
+        var pal = elem.split(' ').length;
+        fs.appendFile('./fileTest', '\n Palabras: ' + pal, function (err) {
+            if (err) throw err;
+          });
+    });
+
+    console.log(textPlain);
 };
+
 function readFile(){
-    //fs.readFile("./fileTest", function (err, data) {
     var buffer = fs.readFile("./fileTest",callback )
-    
-    
   
 }
 
